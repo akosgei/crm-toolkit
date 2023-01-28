f5fac6fafa6f4d101d9d3f14e977c29dfc74f622
katao <kosgeika@gmail.com>
2023-01-28 20:51:02 +0100

    initialized project

---
+++ src/main/java/com/company/crm/toolkit/crmtoolkit/CrmToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.company.crm.toolkit.crmtoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmToolkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmToolkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/crm/toolkit/crmtoolkit/CrmToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.crm.toolkit.crmtoolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmToolkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
